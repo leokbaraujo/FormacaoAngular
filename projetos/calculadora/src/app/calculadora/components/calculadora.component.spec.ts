972226e00436814c743b37e2b334b10827e33b09
Leonel Araujo <Leonel Araujo>
2018-03-06 07:31:33 -0300

    Testando as outras operacoes matemáticas

--- projetos/calculadora/src/app/calculadora/components/calculadora.component.spec.ts
+++ projetos/calculadora/src/app/calculadora/components/calculadora.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { CalculadoraComponent } from './calculadora.component';
 import { CalculadoraService } from '../services';
@@ -21,7 +22,96 @@ describe('CalculadoraComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('deve criar o componente', () => {
     expect(component).toBeTruthy();
   });
+
+  it('deve garantir que 3 + 2 = 5', () => {
+    let btn3 = fixture.debugElement.query(By.css('#btn3'))
+    let btnSoma = fixture.debugElement.query(By.css('#btnSoma'))
+    let btn2 = fixture.debugElement.query(By.css('#btn2'))
+    let btnCalcular = fixture.debugElement.query(By.css('#btnCalcular'))
+    let display = fixture.debugElement.query(By.css('#display'))
+
+    btn3.triggerEventHandler('click', null)
+    fixture.detectChanges();
+
+    btnSoma.triggerEventHandler('click', null)
+    fixture.detectChanges();
+
+    btn2.triggerEventHandler('click', null)
+    fixture.detectChanges();
+
+    btnCalcular.triggerEventHandler('click', null)
+    fixture.detectChanges();
+
+    expect(display.nativeElement.value).toEqual('5')
+  });
+
+  it('deve garantir que 3 - 2 = 1', () => {
+    let btn3 = fixture.debugElement.query(By.css('#btn3'))
+    let btnSubtracao = fixture.debugElement.query(By.css('#btnSubtracao'))
+    let btn2 = fixture.debugElement.query(By.css('#btn2'))
+    let btnCalcular = fixture.debugElement.query(By.css('#btnCalcular'))
+    let display = fixture.debugElement.query(By.css('#display'))
+
+    btn3.triggerEventHandler('click', null)
+    fixture.detectChanges();
+
+    btnSubtracao.triggerEventHandler('click', null)
+    fixture.detectChanges();
+
+    btn2.triggerEventHandler('click', null)
+    fixture.detectChanges();
+
+    btnCalcular.triggerEventHandler('click', null)
+    fixture.detectChanges();
+
+    expect(display.nativeElement.value).toEqual('1')
+  });
+
+  it('deve garantir que 3 / 2 = 1.5', () => {
+    let btn3 = fixture.debugElement.query(By.css('#btn3'))
+    let btnDivisao = fixture.debugElement.query(By.css('#btnDivisao'))
+    let btn2 = fixture.debugElement.query(By.css('#btn2'))
+    let btnCalcular = fixture.debugElement.query(By.css('#btnCalcular'))
+    let display = fixture.debugElement.query(By.css('#display'))
+
+    btn3.triggerEventHandler('click', null)
+    fixture.detectChanges();
+
+    btnDivisao.triggerEventHandler('click', null)
+    fixture.detectChanges();
+
+    btn2.triggerEventHandler('click', null)
+    fixture.detectChanges();
+
+    btnCalcular.triggerEventHandler('click', null)
+    fixture.detectChanges();
+
+    expect(display.nativeElement.value).toEqual('1.5')
+  });
+
+  it('deve garantir que 3 * 2 = 6', () => {
+    let btn3 = fixture.debugElement.query(By.css('#btn3'))
+    let btnMultiplicacao = fixture.debugElement.query(By.css('#btnMIltiplicacao'))
+    let btn2 = fixture.debugElement.query(By.css('#btn2'))
+    let btnCalcular = fixture.debugElement.query(By.css('#btnCalcular'))
+    let display = fixture.debugElement.query(By.css('#display'))
+
+    btn3.triggerEventHandler('click', null)
+    fixture.detectChanges();
+
+    btnMultiplicacao.triggerEventHandler('click', null)
+    fixture.detectChanges();
+
+    btn2.triggerEventHandler('click', null)
+    fixture.detectChanges();
+
+    btnCalcular.triggerEventHandler('click', null)
+    fixture.detectChanges();
+
+    expect(display.nativeElement.value).toEqual('6')
+  });
+
 });
