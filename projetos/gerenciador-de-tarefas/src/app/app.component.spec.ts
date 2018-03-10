73253bd0afc2c61266a2c0750d0b50258a6dac3f
Leonel Araujo <Leonel Araujo>
2018-03-10 15:35:31 -0300

    Ajustando os testes para passar

--- projetos/gerenciador-de-tarefas/src/app/app.component.spec.ts
+++ projetos/gerenciador-de-tarefas/src/app/app.component.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
+
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -6,6 +8,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
